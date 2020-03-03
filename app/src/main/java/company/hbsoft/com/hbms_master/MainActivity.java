37ccd532fc48bc38bcc9e08179ade692bb3ade5b
HeJiuGuang <314765669@qq.com>
2020-03-03 10:48:04 +0800

    Initial commit

---
+++ app/src/main/java/company/hbsoft/com/hbms_master/MainActivity.java
@@ -0,0 +1,46 @@
+package company.hbsoft.com.hbms_master;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import company.hbsoft.com.hbms_master.activity.LoginActivity;
+import company.hbsoft.com.hbms_master.bean.QuestionBean;
+
+public class MainActivity extends AppCompatActivity
+{
+    private List<QuestionBean> mDate=new ArrayList<> ();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+    }
+
+    public void flyto(View view)
+    {
+
+        Toast.makeText (getApplicationContext (),"稍后就出来",Toast.LENGTH_SHORT).show ();
+
+      //   startActivity (new Intent (this, LoginActivity.class));
+
+    }
+
+
+    public class UserLoginTask extends AsyncTask<Void, Void, Boolean>{
+
+
+        @Override
+        protected Boolean doInBackground(Void... voids)
+        {
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/company/hbsoft/com/hbms_master/bean/QuestionBean.java
@@ -0,0 +1,43 @@
+package company.hbsoft.com.hbms_master.bean;
+
+/**
+ * Created by Jiu on 2020/3/2.
+ */
+
+public class QuestionBean
+{
+    private String title;
+    private String cotent;
+    private String isflag;
+
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getCotent()
+    {
+        return cotent;
+    }
+
+    public void setCotent(String cotent)
+    {
+        this.cotent = cotent;
+    }
+
+    public String getIsflag()
+    {
+        return isflag;
+    }
+
+    public void setIsflag(String isflag)
+    {
+        this.isflag = isflag;
+    }
+}
